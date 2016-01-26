b5baabe685d61de5668d2e9e8ab1173fb48c35ac
MohammedMostafaDataServe <moh_mustafa2010@yahoo.com>
2016-01-26 10:24:03 +0300

    commit by Master

---
+++ src/gitapp/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitapp;
+
+/**
+ *
+ * @author Mohammed
+ */
+public class Test {
+
+    public String sayHello(String master) {
+        return "Hello: " + master;
+    }
+}
